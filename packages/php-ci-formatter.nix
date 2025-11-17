e68de5bcfbcf17e020403a0adfbc4a1916f1cf45
alanfzf <29936954+alanfzf@users.noreply.github.com>
2025-11-17 17:15:59 -0600

    build(nix): adjust nix build

--- packages/php-ci-formatter.nix
+++ packages/php-ci-formatter.nix
@@ -1,11 +1,21 @@
 { pkgs, system }:
-
-pkgs.dockerTools.buildLayeredImage {
+let
+in
+pkgs.dockerTools.buildImage {
   name = "php-ci-formatter";
   tag = "latest";
 
-  contents = [
-    pkgs.php84Packages.php-cs-fixer
-    pkgs.prettier
-  ];
+  # TODO: https://github.com/actions/checkout/issues/334#issuecomment-2009585837
+
+  copyToRoot = pkgs.buildEnv {
+    name = "php-ci-formatter";
+    paths = [
+      pkgs.dockerTools.usrBinEnv
+      pkgs.dockerTools.binSh
+      pkgs.coreutils
+      # pkgs.bash
+      # pkgs.prettier
+      # pkgs.php84Packages.php-cs-fixer
+    ];
+  };
 }
